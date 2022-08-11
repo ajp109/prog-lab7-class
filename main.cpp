13e77eba6fb162d1992e1ad737b71d70c8184b91
ajp109 <57534418+ajp109@users.noreply.github.com>
2022-08-11 11:58:17 +0100

    Correct code in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,46 @@
 #include "mbed.h"
 
-int main() {
-    while (true) {
-        sleep();
+class Flasher {
+
+    DigitalOut pin_;
+    Ticker ticker_;
+    Timeout timeout_;
+    chrono::milliseconds onTime_;
+    chrono::milliseconds offTime_;
+
+    void on() {
+        pin_ = true;
+        timeout_.attach(callback(this, &Flasher::off), onTime_);
     }
+
+    void off() {
+        pin_ = false;
+    }
+
+public:
+
+    Flasher(PinName pin, chrono::milliseconds const & onTime, chrono::milliseconds const & offTime) :
+        pin_(pin), onTime_(onTime), offTime_(offTime) { }
+
+    void start() {
+        ticker_.attach(callback(this, &Flasher::on), onTime_ + offTime_);
+    }
+
+    void stop() {
+        ticker_.detach();
+    }
+
+};
+
+
+
+Flasher green(D2, 500ms, 2500ms);
+Flasher red(D3, 300ms, 1700ms);
+
+int main()
+{
+    green.start();
+    red.start();
+    // This while loop is important - it's there to stop the program from ending
+    while (true);
 }
